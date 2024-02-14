14b11f4c8db067dd65c8641cebfdccd11a29d043
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-02-15 03:23:18 +0530

    feat: link removed

--- components/globals/header.tsx
+++ components/globals/header.tsx
@@ -12,17 +12,11 @@ export function Header() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-5">
-            <Link
-              href={siteConfig.links.contribute}
-              target="_blank"
-              rel="noreferrer"
-            >
               <div className="bg-supabase border border-[#70CC93] rounded-sm">
                 <div className="inline-flex items-center py-1.5 px-6 space-x-3 rounded-md text-white">
                   <p className="font-semibold">Register</p>
                 </div>
               </div>
-            </Link>
             <Link href="https://github.com/nermalcat69/supabase-ssr-starter" target="_blank" className="text-gray-100 hover:text-gray-200 text-xl">
             <FaGithub />
             </Link>
