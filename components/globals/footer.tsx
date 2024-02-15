f56fc8942191a282c311579a331f70bb1ffc6dfb
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-02-15 23:35:29 +0530

    feat: ui improvement

--- components/globals/footer.tsx
+++ components/globals/footer.tsx
@@ -7,11 +7,11 @@ export function Footer() {
       <div className="container flex items-center justify-center py-4">
         <span className="text-[#DEDEDE]">
           Powered by{" "}
-          <span className="font-bold hover:text-gray-600 cursor-pointer">
+          <span className="font-bold hover:text-[#e9e9e9] cursor-pointer">
             Supabase{" "}
           </span>
           &{" "}
-          <span className="font-bold hover:text-gray-600 cursor-pointer">
+          <span className="font-bold hover:text-[#e9e9e9] cursor-pointer">
             Vercel
           </span>
         </span>
--- components/globals/hello.tsx
+++ components/globals/hello.tsx
@@ -3,7 +3,7 @@ export function Hello() {
 
   return (
     <div className="fixed hidden md:block bottom-20 right-10 z-50  px-5 py-3 items-center justify-center rounded-full bg-secondarybg p-3 font-mono text-xs text-white duration-100 hover:bg-neutral-700 cursor-pointer">
-      <div className="hidden lg:block font-bold">Hello ^_^ </div>
+      <div className="hidden lg:block font-bold">You’re Not Logged In :3 </div>
     </div>
   );
 }
