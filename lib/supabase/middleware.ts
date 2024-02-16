39ef4e7370f819530b65cd3cd129e6e56245a1da
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-02-16 09:28:19 +0530

    feat: env changes

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -10,8 +10,8 @@ export const createClient = (request: NextRequest) => {
   });
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,8 +4,8 @@ import "server-only";
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
